94c40a1dc37268df67c0d2a626814af86e23476b
Nitesh <jobkomail@gmail.com>
2022-01-14 12:05:38 +1100

    JPA modelling

---
+++ src/main/java/com/example/recipe/domain/UnitOfMeasure.java
@@ -0,0 +1,21 @@
+package com.example.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
